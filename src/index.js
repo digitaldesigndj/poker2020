03f21452ae6fdd18d1d36286cb8652b512e72dcc
Taylor <tdy721@gmail.com>
2020-05-10 18:47:52 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log( 'hello console' );
+
+function component() {
+    const element = document.createElement('div');
+    element.innerHTML = ['Hello', 'webpack', " this is a website!"].join(' ');
+    return element;
+}
+document.body.appendChild(component());
+
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+  module.exports = {
+  mode: 'development',
+    entry: {
+      app: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+   plugins: [
+    new HtmlWebpackPlugin({
+       title: 'Output Management',
+     }),
+   ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+  };
